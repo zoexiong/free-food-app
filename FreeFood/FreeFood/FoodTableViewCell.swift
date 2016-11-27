1cd5c1d50c63ea4e74de86efcf89d61b92804076
Xuan liu <xuanliu@uw.edu>
2016-11-27 13:37:18 -0800

    basic screens for navigation with corresponding swift files

---
+++ FreeFood/FreeFood/FoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodTableViewCell.swift
+//  FreeFood
+//
+//  Created by Xuan Liu on 2016/11/27.
+//  Copyright © 2016年 Xuan Liu. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
