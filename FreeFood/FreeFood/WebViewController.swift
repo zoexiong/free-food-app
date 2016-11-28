a6cb1b4d92424365577baf9a416d32045ae45c0d
Xuan liu <xuanliu@uw.edu>
2016-11-27 16:48:40 -0800

    web view

---
+++ FreeFood/FreeFood/WebViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WebViewController.swift
+//  FreeFood
+//
+//  Created by Xuan Liu on 2016/11/27.
+//  Copyright © 2016年 Xuan Liu. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
